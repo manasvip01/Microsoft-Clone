d17ec88fd6ddd07736ccb8fee57c3a27ae73ad22
Manasvi Prakash <manasviprakash01@gmail.com>
2022-06-14 23:04:26 +0530

    message

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        xs:'285px',
+        s:'310px',
+        d: '768px',
+      }
+    },
+  },
+  plugins: [],
+}
